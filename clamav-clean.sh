2db21ae8c4e7d307475780b252193f5b77e93045
Sérgio M. Basto <sergio@serjux.com>
2018-10-04 13:19:12 +0100

    Add gpg verify to clamav-clean.sh script

--- clamav-clean.sh
+++ clamav-clean.sh
@@ -4,6 +4,8 @@ TARBALL_CLEAN=${NAME}-${VERSION}-norar.tar.xz
 TARBALL=${NAME}-${VERSION}.tar.gz
 
 wget https://www.clamav.net/downloads/production/${TARBALL}
+wget https://www.clamav.net/downloads/production/${TARBALL}.sig
+gpg --verify ${TARBALL}.sig ${TARBALL}
 #rm -f ${TARBALL}.tmp
 zcat ${TARBALL} | tar --delete -f - '*/libclamunrar/*' | xz -c > ${TARBALL_CLEAN}
 #mv ${TARBALL}.tmp ${TARBALL_CLEAN}
