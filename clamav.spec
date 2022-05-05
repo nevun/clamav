fd5e7f3d2fcb643e8e2875ead10e5cdb833034aa
Sérgio M. Basto <sergio@serjux.com>
2022-05-05 23:35:30 +0100

    Update to 0.103.6

--- clamav.spec
+++ clamav.spec
@@ -33,8 +33,8 @@
 
 Summary:    End-user tools for the Clam Antivirus scanner
 Name:       clamav
-Version:    0.103.5
-Release:    3%{?dist}
+Version:    0.103.6
+Release:    1%{?dist}
 License:    %{?with_unrar:proprietary}%{!?with_unrar:GPLv2}
 URL:        https://www.clamav.net/
 %if %{with unrar}
@@ -56,7 +56,7 @@ Source5:    clamd-README
 #http://database.clamav.net/main.cvd
 Source10:   main-62.cvd
 #http://database.clamav.net/daily.cvd
-Source11:   daily-26420.cvd
+Source11:   daily-26532.cvd
 #http://database.clamav.net/bytecode.cvd
 Source12:   bytecode-333.cvd
 #for update
@@ -79,9 +79,6 @@ Patch0:     clamav-stats-deprecation.patch
 Patch1:     clamav-default_confs.patch
 # Fix pkg-config flags for static linking, multilib
 Patch2:     clamav-0.99-private.patch
-# Fix ck_assert_msg() call
-# https://github.com/Cisco-Talos/clamav-devel/pull/138
-Patch4:     clamav-check.patch
 # Modify clamav-clamonacc.service for Fedora compatibility
 Patch5:     clamav-clamonacc-service.patch
 
@@ -252,7 +249,6 @@ This package contains files which are needed to run the clamav-milter.
 %endif
 %patch1 -p1 -b .default_confs
 %patch2 -p1 -b .private
-%patch4 -p1 -b .check
 %patch5 -p1 -b .clamonacc-service
 %patch6 -p1 -b .freshclam-service
 
@@ -580,6 +576,9 @@ test -e %{freshclamlog} || {
 
 
 %changelog
+* Thu May 05 2022 Sérgio Basto <sergio@serjux.com> - 0.103.6-1
+- Update to 0.103.6
+
 * Tue Mar 01 2022 Sérgio Basto <sergio@serjux.com> - 0.103.5-3
 - Fix for dnf update clamav-update (#2059618)
 
--- update_clamav.sh
+++ update_clamav.sh
@@ -1,4 +1,5 @@
-VERSION=0.103.5
+VERSION=0.103.6
+REPOS="f36 f35 f34 epel9 epel8 epel7"
 
 if [ -z "$1" ]
 then
@@ -11,25 +12,39 @@ NAME=clamav
 TARBALL_CLEAN=${NAME}-${VERSION}-norar.tar.xz
 TARBALL=${NAME}-${VERSION}.tar.gz
 
+echo "Usage: $0 stage"
+echo "stage 0: prepare sources"
+echo "stage 1: get cvd (not working at all) the donwload needs to be done manually"
+echo "stage 2: upload sources and ask for scratch build"
+echo "stage 3: push and build on rawhide"
+echo "stage 4: build on others branches"
+echo ""
+
 if test $stage -le 0
 then
 echo STAGE 0
+echo Press enter to prepare sources or n to skip ; read dummy;
+if [[ "$dummy" != "n" ]]; then
 wget -c https://www.clamav.net/downloads/production/${TARBALL}
 wget -c https://www.clamav.net/downloads/production/${TARBALL}.sig
 gpg --verify ${TARBALL}.sig ${TARBALL}
 zcat ${TARBALL} | tar --delete -f - '*/libclamunrar/*' | xz -c > ${TARBALL_CLEAN}
 fi
+fi
 
-echo Press enter convert cvd into spec or n to skip ; read dummy;
-if [[ "$dummy" != "n" ]]; then
 # WIP clouflare don't allow wget we need download with browser
 #wget https://database.clamav.net/main.cvd
 #wget https://database.clamav.net/daily.cvd
 #wget https://database.clamav.net/bytecode.cvd
-
 main_ver=$(file main.cvd | sed -e 's/.*version /main-/;s/,.*/.cvd/')
 daily_ver=$(file daily.cvd | sed -e 's/.*version /daily-/;s/,.*/.cvd/')
 bytecode_ver=$(file bytecode.cvd | sed -e 's/.*version /bytecode-/;s/,.*/.cvd/')
+
+if test $stage -le 1
+then
+echo STAGE 1
+echo Press enter convert cvd into spec or n to skip ; read dummy;
+if [[ "$dummy" != "n" ]]; then
 
 cp -f main.cvd $main_ver
 cp -f daily.cvd $daily_ver
@@ -39,7 +54,11 @@ sed -i "s|^Source10: .*|Source10:   $main_ver|" clamav.spec
 sed -i "s|^Source11: .*|Source11:   $daily_ver|" clamav.spec
 sed -i "s|^Source12: .*|Source12:   $bytecode_ver|" clamav.spec
 fi
+fi
 
+if test $stage -le 2
+then
+echo STAGE 2
 rpmdev-bumpspec -n $VERSION -c "Update to $VERSION" clamav.spec
 echo fedpkg new-sources ${TARBALL_CLEAN} $main_ver $daily_ver $bytecode_ver
 echo Press enter scratch-build or n to skip ; read dummy;
@@ -47,16 +66,28 @@ if [[ "$dummy" != "n" ]]; then
 #fkinit -u sergiomb
 fedpkg scratch-build --srpm
 fi
-echo Press enter to upload sources and commit ; read dummy;
+echo Press enter to upload sources and commit or n to skip; read dummy;
+if [[ "$dummy" != "n" ]]; then
 fedpkg new-sources ${TARBALL_CLEAN} $(spectool -l clamav.spec | grep -P "Source10|Source11|Source12" | sed 's/.* //')
 fedpkg ci -c && git show
+fi
+fi
 
-echo Press enter to build rawhide; read dummy;
+if test $stage -le 3
+then
+echo STAGE 3
+echo Press enter to build rawhide or n to skip; read dummy;
+if [[ "$dummy" != "n" ]]; then
 git push && fedpkg build --nowait
+fi
+fi
 
-for repo in "f35 f34 epel8 epel7" ; do
-echo Press enter to build on branch $repo; read dummy;
+echo STAGE 4
+for repo in $REPOS ; do
+echo Press enter to build on branch $repo or n to skip; read dummy;
+if [[ "$dummy" != "n" ]]; then
 git checkout $repo && git merge rawhide && fedpkg push && fedpkg build --nowait; git checkout rawhide
+fi
 done
 
 exit
